3d23041978a41b41a2e03cf1e345cb0a813c9b41
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-26 00:02:56 +0200

    feat: add header in admin

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -6,15 +6,25 @@
         <div>
             <ul class="navbar">
                 @guest
-                    <li class="nav-item"><a href="{{ route('login') }}">Login</a></li>
+                    <li class="nav-item pe-3"><a href="{{ route('login') }}">Login</a></li>
                     <li class="nav-item"><a href="{{ route('register') }}">Registrati</a></li>
                 @else
                     <li class="nav-item dropdown">
-                        <a href="#">{{ Auth::user()->name }}</a>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a href="{{ route('admin.home') }}">Admin</a>
-                            <a href="{{ route('logout') }}">Logout</a>
+                        <div class="dropdown-menu dropdown-menu " aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
 
 
                         </div>
